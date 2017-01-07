c154f5ae5002e0803d354d7b487a55c7cac7b8e3
Brian Cloutier <github@briancloutier.com>
2017-01-07 18:11:24 +0300

    Add task to run Citus' regression tests

--- fabfile/run.py
+++ fabfile/run.py
@@ -1,4 +1,6 @@
-from fabric.api import task, run, cd, runs_once
+from fabric.api import task, run, cd, runs_once, roles
+
+import config
 
 @task
 @runs_once
@@ -7,3 +9,10 @@ def jdbc():
     with cd('/home/ec2-user/test-automation/jdbc'):
         run('javac JDBCReleaseTest.java')
         run('java -classpath postgresql-9.4.1212.jre6.jar:. JDBCReleaseTest')
+
+@task
+@roles('master')
+def regression():
+    'Runs Citus\' regression tests'
+    with cd(config.paths['citus-repo']):
+        run('make check')

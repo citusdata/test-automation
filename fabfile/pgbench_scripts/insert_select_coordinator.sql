982b801c0e10108e83707c21782e8bcc649f01a1
metdos <metin@citusdata.com>
2018-01-11 17:18:59 +0200

    Fix INSERT INTO ... SELECT via coordinator

--- fabfile/pgbench_scripts/insert_select_coordinator.sql
+++ fabfile/pgbench_scripts/insert_select_coordinator.sql
@@ -1,3 +1,3 @@
 \set aid  random(1, 100000)
 \set bid  random(1, 100000)
-INSERT INTO test_table_target SELECT * FROM test_table WHERE  :bid < 1000 LIMIT 10;
+INSERT INTO test_table_target SELECT * FROM test_table WHERE  key = :aid OR key =:bid LIMIT 10;

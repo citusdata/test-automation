4a87fc76a60370a7f1eb1ea7af07762cc7733052
SaitTalhaNisanci <s.talhanisanci@gmail.com>
2019-10-01 13:38:18 +0300

    use os path join instead of hard coded joins

--- fabfile/config.py
+++ fabfile/config.py
@@ -1,16 +1,17 @@
 from os.path import expanduser
+import os
 
 HOME_DIR = expanduser("~")
 
 paths = {
-    'code-directory': HOME_DIR + '/code',
-    'tests-repo': HOME_DIR + '/test-automation',
-    'citus-repo': HOME_DIR + '/citus',
-    'enterprise-repo': HOME_DIR + '/citus-enterprise',
-    'pg-latest': HOME_DIR + '/pg-latest',
-    'pg-source-balls': HOME_DIR + '/postgres-source',
+    'code-directory': os.path.join(HOME_DIR, 'code'),
+    'tests-repo': os.path.join(HOME_DIR, 'test-automation'),
+    'citus-repo': os.path.join(HOME_DIR, 'citus'),
+    'enterprise-repo': os.path.join(HOME_DIR, 'citus-enterprise'),
+    'pg-latest': os.path.join(HOME_DIR, 'pg-latest'),
+    'pg-source-balls': os.path.join(HOME_DIR, 'postgres-source'),
     'home-directory': HOME_DIR, 
-    'citus-installation': HOME_DIR + '/citus-installation'
+    'citus-installation': os.path.join(HOME_DIR, 'citus-installation')
 }
 
 settings = {

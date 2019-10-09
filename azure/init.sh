03d6d6daa80a08589302f6d725b54a88545260de
SaitTalhaNisanci <s.talhanisanci@gmail.com>
2019-10-09 21:59:42 +0300

    Add custom script to initialize vms after startup

--- azure/init.sh
+++ azure/init.sh
@@ -1,17 +1,27 @@
 #!/bin/bash
 
-echo 'export PATH=${HOME}/pg-latest/bin/:$PATH' >> ${HOME}/.bashrc
+sudo yum update -y 
+
+curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
+sudo python get-pip.py
+
+sudo yum install -y \
+    git 
+
+sudo su pguser  
+
+echo 'pguser     ALL=(ALL) NOPASSWD:ALL' >> /etc/sudoers     
+
+echo 'export PATH=${HOME}/pg-latest/bin/:$PATH' >> ${HOME}/.bash_profile
 
 git clone https://github.com/citusdata/test-automation.git
 
 ln -s ${HOME}/test-automation/fabfile ${HOME}/fabfile
 
 echo 'source ${HOME}/test-automation/cloudformation/fab' >> ${HOME}/.bashrc
-echo 'pguser     ALL=(ALL) NOPASSWD:ALL' >> /etc/sudoers 
 
-sudo apt-get update && sudo apt install -y \
-    python-pip \
-    yum
+pip install -r ${HOME}/test-automation/fabfile/requirements.txt --user
 
-pip install -r ${HOME}/test-automation/fabfile/requirements.txt
+echo | ssh-keygen -P '' -t rsa
+cat ~/.ssh/id_rsa.pub >> ~/.ssh/authorized_keys
 

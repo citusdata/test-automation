20fabfa4623aa400f632666d05c6d4de62474444
SaitTalhaNisanci <s.talhanisanci@gmail.com>
2019-10-09 23:03:41 +0300

    update script and add deploy parameters

--- azure/init.sh
+++ azure/init.sh
@@ -8,20 +8,22 @@ sudo python get-pip.py
 sudo yum install -y \
     git 
 
-sudo su pguser  
+echo 'pguser     ALL=(ALL) NOPASSWD:ALL' >> /etc/sudoers
 
-echo 'pguser     ALL=(ALL) NOPASSWD:ALL' >> /etc/sudoers     
+sudo su pguser       
 
 echo 'export PATH=${HOME}/pg-latest/bin/:$PATH' >> ${HOME}/.bash_profile
+echo 'export PATH=${HOME}/.local/bin/:$PATH' >> ${HOME}/.bash_profile
 
 git clone https://github.com/citusdata/test-automation.git
 
 ln -s ${HOME}/test-automation/fabfile ${HOME}/fabfile
 
-echo 'source ${HOME}/test-automation/cloudformation/fab' >> ${HOME}/.bashrc
+echo 'source ${HOME}/test-automation/cloudformation/fab' >> ${HOME}/.bash_profile
 
 pip install -r ${HOME}/test-automation/fabfile/requirements.txt --user
 
 echo | ssh-keygen -P '' -t rsa
 cat ~/.ssh/id_rsa.pub >> ~/.ssh/authorized_keys
+chmod 600 ~/.ssh/authorized_keys
 

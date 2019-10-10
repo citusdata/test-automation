ea23de3111bdfe37aa08688a09327bd923206aa2
SaitTalhaNisanci <s.talhanisanci@gmail.com>
2019-10-10 14:27:54 +0300

    update init script

--- azure/init.sh
+++ azure/init.sh
@@ -1,27 +1,28 @@
 #!/bin/bash
 
-curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
-sudo python get-pip.py
+# in redhat we need to enable default port for postgres
+firewall-cmd --add-port=5432/tcp
 
-sudo yum install -y \
-    git 
+curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
+python get-pip.py
+
+yum install -y \
+    git
 
 echo 'pguser     ALL=(ALL) NOPASSWD:ALL' >> /etc/sudoers
 
-sudo su pguser       
+su - pguser << 'EOSU'
+echo "export PATH=${HOME}/pg-latest/bin/:${HOME}/.local/bin/:$PATH" >> ${HOME}/.bash_profile
 
-echo 'export PATH=${HOME}/pg-latest/bin/:$PATH' >> ${HOME}/.bash_profile
-echo 'export PATH=${HOME}/.local/bin/:$PATH' >> ${HOME}/.bash_profile
-
-git clone https://github.com/citusdata/test-automation.git
+cd ${HOME} && git clone https://github.com/citusdata/test-automation.git
 
 ln -s ${HOME}/test-automation/fabfile ${HOME}/fabfile
 
-echo 'source ${HOME}/test-automation/cloudformation/fab' >> ${HOME}/.bash_profile
-
+echo "source ${HOME}/test-automation/cloudformation/fab" >> ${HOME}/.bash_profile
 pip install -r ${HOME}/test-automation/fabfile/requirements.txt --user
 
-echo | ssh-keygen -P '' -t rsa
+echo | ssh-keygen -P "" -t rsa
 cat ~/.ssh/id_rsa.pub >> ~/.ssh/authorized_keys
 chmod 600 ~/.ssh/authorized_keys
 
+EOSU

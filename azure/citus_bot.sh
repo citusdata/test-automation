0bb71d42c2dc2f54fa95af219360c7e69891726a
SaitTalhaNisanci <s.talhanisanci@gmail.com>
2019-10-16 01:16:15 +0300

    run tests with screen

--- azure/citus_bot.sh
+++ azure/citus_bot.sh
@@ -7,7 +7,7 @@ set -e
 # fail in a pipeline if any of the commands fails
 set -o pipefail
 
-rg=citusbot_test_resource_group3
+rg=citusbot_test_resource_group1
 export RESOURCE_GROUP_NAME=${rg}
 sh ./create-cluster.sh ${rg}
 
@@ -19,9 +19,12 @@ public_ip=$(echo ${public_ip} | cut -d "\"" -f 2)
 echo ${public_ip}
 ssh-keyscan -H ${public_ip} >> ~/.ssh/known_hosts
 
+# echo "Host *" >> /etc/ssh/ssh_config
+# echo "ServerAliveInterval 120" >> /etc/ssh/ssh_config
+
 sh ./delete_security_rule.sh
 
 # ssh with non-interactive mode does not source bash profile, so we will need to do it ourselves here.
-ssh -A pguser@${public_ip} "source ~/.bash_profile; sh /home/pguser/test-automation/azure/run_all_tests.sh"
+ssh -A pguser@${public_ip} screen -dm bash -c "source ~/.bash_profile; sh /home/pguser/test-automation/azure/run_all_tests.sh"
 
 # sh ./delete_resource_group.sh ${rg}

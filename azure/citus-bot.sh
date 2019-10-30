374f233bb23b6823b2dbd25d482391a170cb390a
SaitTalhaNisanci <s.talhanisanci@gmail.com>
2019-10-30 11:21:21 +0300

    rename scripts

--- azure/citus-bot.sh
+++ azure/citus-bot.sh
@@ -10,7 +10,7 @@ set -x
 
 rg=citusbot_test_resource_group
 export RESOURCE_GROUP_NAME=${rg}
-sh ./create-cluster.sh
+./create-cluster.sh
 
 
 public_ip=$(az group deployment show -g ${rg} -n azuredeploy --query properties.outputs.publicIP.value)
@@ -30,6 +30,6 @@ echo "adding public ip to known hosts in remote"
 ssh -o "StrictHostKeyChecking no" -A pguser@${public_ip} "ssh-keyscan -H ${public_ip} >> /home/pguser/.ssh/known_hosts"
 echo "running tests in remote"
 # ssh with non-interactive mode does not source bash profile, so we will need to do it ourselves here.
-ssh -o "StrictHostKeyChecking no" -A pguser@${public_ip} "source ~/.bash_profile;sh /home/pguser/test-automation/azure/run_all_tests.sh"
+ssh -o "StrictHostKeyChecking no" -A pguser@${public_ip} "source ~/.bash_profile;sh /home/pguser/test-automation/azure/run-all-tests.sh"
 
 # sh ./delete_resource_group.sh ${rg}

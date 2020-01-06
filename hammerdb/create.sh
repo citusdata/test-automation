c3ec7a96d5afdfc070177f1903ef0ac97f6b79a0
SaitTalhaNisanci <s.talhanisanci@gmail.com>
2020-01-06 12:34:09 +0300

    use current branch by default to clone repo

--- hammerdb/create.sh
+++ hammerdb/create.sh
@@ -28,11 +28,16 @@ public_key=$(cat ~/.ssh/id_rsa.pub)
 start_time=`date +%s`
 echo "waiting a long time to create cluster, this might take up to 30 mins depending on your cluster size"
 
-# if this is run on a job, use the branch for the job, otherwise use master(running in local or remote without a job)
+# https://stackoverflow.com/questions/1593051/how-to-programmatically-determine-the-current-checked-out-git-branch
+branch_name=$(git symbolic-ref -q HEAD)
+branch_name=${branch_name##refs/heads/}
+branch_name=${branch_name:-HEAD}
+
+# if this is run on a job, use the branch for the job, otherwise use the local(running in local or remote without a job)
 # store the branch name in a file so that target user can read it. Target user cannot see the envionment variables because
 # we use login option in su and -p(preserving environment variables) cannot be used with login. We need to use login option
 # so that $HOME, $PATH are set to the target users $HOME and $PATH.
-export BRANCH=${CIRCLE_BRANCH:=hammerdb}
+export BRANCH=${CIRCLE_BRANCH:=$branch_name}
 
 az group deployment create -g ${rg} --template-file azuredeploy.json --parameters @azuredeploy.parameters.json --parameters sshPublicKey="${public_key}" branchName="$BRANCH"
 

df21d94b24f8709125795319ac5c566eeb4929c4
SaitTalhaNisanci <s.talhanisanci@gmail.com>
2020-01-06 17:49:22 +0300

    install hammerdb from fork

--- hammerdb/setup.sh
+++ hammerdb/setup.sh
@@ -25,8 +25,12 @@ cp tpcc-distribute.sql ${hammerdb_dir}/
 cp tpcc-distribute-funcs.sql ${hammerdb_dir}/
 cp drop-tables.sql ${hammerdb_dir}/ 
 
+# cd ${HOME}
+# git clone --branch citus https://github.com/SaitTalhaNisanci/HammerDB.git
+# mv HammerDB/src/postgresql/pgoltp.tcl HammerDB-3.3/src/postgresql/pgoltp.tcl
+
 cd ${hammerdb_dir}/src/postgresql
-# comment out create database and user as citus cannot do that
+comment out create database and user as citus cannot do that
 sed -i 's/CreateUserDatabase $lda $db $superuser $user $password/#CreateUserDatabase $lda $db $superuser $user $password/g' pgoltp.tcl
 
 sudo yum -y install https://download.postgresql.org/pub/repos/yum/reporpms/EL-7-x86_64/pgdg-redhat-repo-latest.noarch.rpm

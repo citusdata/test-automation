3f7ce3cbc9d2cd497b24a7d1270b10cb7d2ef2c5
Onur Tirtir <onurcantirtir@gmail.com>
2020-01-08 19:07:07 +0300

    add ci jobs and add a new script for valgrind test

--- azure/citus-bot.sh
+++ azure/citus-bot.sh
@@ -7,17 +7,24 @@ set -e
 # echo commands
 set -x
 
-
 function cleanup {
     sh ./delete-resource-group.sh
 }
 
-trap cleanup EXIT
-
 rg=$1
 export RESOURCE_GROUP_NAME=${rg}
-./create-cluster.sh
 
+# If running valgrind tests, do not run cleanup function
+# This is because, as valgrind tests requires too much time to run,
+# we start valgrind tests in a tmux session in ci. Hence ssh session 
+# will immediately be closed just after the fabric command is run
+#
+# We have a seperate job to terminate the machine and push the results
+if [ "$rg" != "citusbot_valgrind_test_resource_group" ]; then
+    trap cleanup EXIT
+fi 
+
+./create-cluster.sh
 
 public_ip=$(az group deployment show -g ${rg} -n azuredeploy --query properties.outputs.publicIP.value)
 # remove the quotes 
---
+++ azure/finalize-valgrind-test.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+# fail if trying to reference a variable that is not set.
+set -u
+# exit immediately if a command fails
+set -e
+# echo commands
+set -x
+
+function cleanup {
+    sh ./delete-resource-group.sh
+}
+
+rg="citusbot_valgrind_test_resource_group"
+
+trap cleanup EXIT
+
+public_ip=$(az group deployment show -g ${rg} -n azuredeploy --query properties.outputs.publicIP.value)
+# remove the quotes 
+public_ip=$(echo ${public_ip} | cut -d "\"" -f 2)
+echo ${public_ip}
+ssh-keyscan -H ${public_ip} >> ~/.ssh/known_hosts
+chmod 600 ~/.ssh/known_hosts
+
+sh ./delete-security-rule.sh
+
+echo "adding public ip to known hosts in remote"
+ssh -o "StrictHostKeyChecking no" -A pguser@${public_ip} "ssh-keyscan -H ${public_ip} >> /home/pguser/.ssh/known_hosts"
+echo "running tests in remote"
+
+# ssh with non-interactive mode does not source bash profile, so we will need to do it ourselves here.
+# put an empty success file for valgrind tests under results dir if there are error logs
+# push the files under results dir
+ssh -o "StrictHostKeyChecking no" -A pguser@${public_ip} \
+"source ~/.bash_profile;" \
+"fab run.valgrind_put_success;" \
+"/home/pguser/test-automation/azure/push-results.sh ${rg};"

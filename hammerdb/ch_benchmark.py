6f0250571a4e4fc797a65b630de8910152ba28c6
SaitTalhaNisanci <s.talhanisanci@gmail.com>
2020-01-16 16:09:12 +0300

    remove the 5th query as it is taking way too slow

--- hammerdb/ch_benchmark.py
+++ hammerdb/ch_benchmark.py
@@ -74,26 +74,6 @@ group    by o_ol_cnt
 order    by o_ol_cnt LIMIT 10;
 """,
 """
-select     n_name,
-     sum(ol_amount) as revenue
-from     customer, orders, order_line, stock, supplier, nation, region
-where     c_id = o_c_id
-     and c_w_id = o_w_id
-     and c_d_id = o_d_id
-     and ol_o_id = o_id
-     and ol_w_id = o_w_id
-     and ol_d_id=o_d_id
-     and ol_w_id = s_w_id
-     and ol_i_id = s_i_id
-     and mod((s_w_id * s_i_id),10000) = su_suppkey
-     and ascii(substr(c_state,1,1)) = su_nationkey
-     and su_nationkey = n_nationkey
-     and n_regionkey = r_regionkey
-     and r_name = 'Europe'
-group by n_name
-order by revenue desc LIMIT 10;
-""",
-"""
 select    sum(ol_amount) as revenue
 from order_line
 where ol_quantity between 1 and 100000 LIMIT 10;

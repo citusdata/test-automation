888fdabb7821377a72a6c8a6c94fc2c85c02520d
SaitTalhaNisanci <s.talhanisanci@gmail.com>
2020-01-26 20:39:16 +0300

    test

--- hammerdb/build.tcl
+++ hammerdb/build.tcl
@@ -17,8 +17,8 @@ diset tpcc pg_defaultdbase pguser
 #diset tpcc pg_superuserpass yourpasswordhere
 # if you change this, make sure to change tpcc-distribute-funcs.sql
 diset tpcc pg_storedprocs true
-diset tpcc pg_num_vu 5
-diset tpcc pg_count_ware 25
+diset tpcc pg_num_vu 250
+diset tpcc pg_count_ware 1000
 diset tpcc pg_total_iterations 1000000
 diset tpcc pg_driver timed
 diset tpcc pg_rampup 3

a94627527fff16bdabbee5c98497da18752e0dfb
SaitTalhaNisanci <s.talhanisanci@gmail.com>
2020-01-27 17:27:08 +0300

    make ssh more tolerant to timeout errors

--- azure/citus-bot.sh
+++ azure/citus-bot.sh
@@ -7,6 +7,22 @@ set -e
 # echo commands
 set -x
 
+ssh_execute() {
+   ip=$1
+   shift;
+   command=$@
+   n=0
+   until [ $n -ge 4 ]
+   do
+      sh ./delete-security-rule.sh
+      ssh -o "StrictHostKeyChecking no" -A pguser@${ip} "source ~/.bash_profile;${command}" && break
+      n=$[$n+1]
+   done
+
+   if [ $n == 4 ]; then
+      exit 1
+   fi
+}
 
 function cleanup {
     sh ./delete-resource-group.sh
@@ -29,8 +45,8 @@ chmod 600 ~/.ssh/known_hosts
 sh ./delete-security-rule.sh
 
 echo "adding public ip to known hosts in remote"
-ssh -o "StrictHostKeyChecking no" -A pguser@${public_ip} "ssh-keyscan -H ${public_ip} >> /home/pguser/.ssh/known_hosts"
+ssh_execute ${public_ip} "ssh-keyscan -H ${public_ip} >> /home/pguser/.ssh/known_hosts"
 echo "running tests in remote"
 # ssh with non-interactive mode does not source bash profile, so we will need to do it ourselves here.
-ssh -o "StrictHostKeyChecking no" -A pguser@${public_ip} "source ~/.bash_profile;/home/pguser/test-automation/azure/run-all-tests.sh ${rg}"
+ssh_execute ${public_ip} "source ~/.bash_profile;/home/pguser/test-automation/azure/run-all-tests.sh ${rg}"
 

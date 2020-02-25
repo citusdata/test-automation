5414e82b88c2b3ec293cdfc25c60490b6722e4c8
SaitTalhaNisanci <s.talhanisanci@gmail.com>
2020-02-25 16:56:31 +0300

    add a utility script to connect to the driver node

---
+++ hammerdb/connect-driver.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# fail if trying to reference a variable that is not set.
+set -u
+# exit immediately if a command fails
+set -e
+# echo commands
+set -x
+
+## Set mydir to the directory containing the script
+## The ${var%pattern} format will remove the shortest match of
+## pattern from the end of the string. Here, it will remove the
+## script's name,. leaving only the directory. 
+dir="${0%/*}"
+cd ${dir}
+
+public_ip=$(az group deployment show -g ${RESOURCE_GROUP_NAME} -n azuredeploy --query properties.outputs.driverPublicIP.value)
+# remove the quotes 
+public_ip=$(echo ${public_ip} | cut -d "\"" -f 2)
+sh ../azure/delete-security-rule.sh
+
+ssh -o "StrictHostKeyChecking no" -A pguser@${public_ip}

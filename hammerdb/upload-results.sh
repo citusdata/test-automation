a141a8518d24684078575774c916a3bc4123a824
SaitTalhaNisanci <s.talhanisanci@gmail.com>
2020-02-25 16:54:10 +0300

    add scripts to run tpcc in an automated way
    In order to be able to run the test in an automated way, screen is used
    to create a detached session, github token and username are cached in
    the driver node so that when it needs to access a private repository, it
    can use the tokens.
    
    The create-and-run.sh script will create the cluster and the driver node
    in the same subnet, so that the network latency will be lower.

---
+++ hammerdb/upload-results.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+# fail if trying to reference a variable that is not set.
+set -u
+# exit immediately if a command fails
+set -e
+# echo commands
+set -x
+
+hammerdb_dir=${HOME}/HammerDB-3.3
+rg_name=$1
+
+cd ${hammerdb_dir}
+
+cp build.tcl ./results
+cp run.tcl ./results
+
+cp -r ${HOME}/test-automation/hammerdb ./results/hammerdb
+
+cd $HOME
+
+# add github to known hosts
+echo "github.com ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAQEAq2A7hRGmdnm9tUDbO9IDSwBK6TbQa+PXYPCPy6rbTrTtw7PHkccKrpp0yVhp5HdEIcKr6pLlVDBfOLX9QUsyCOV0wzfjIJNlGEYsdlLJizHhbn2mUjvSAHQqZETYP81eFzLQNnPHt4EVVUh7VfDESU84KezmD5QlWpXLmvU31/yMf+Se8xhHTvKSCZIFImWwoG6mbUoWf9nzpIoaSjB+weqqUUmpaaasXVal72J+UX2B+2RPW3RcT0eOzQgqlJL3RKrTJvdsjE3JEAvGq3lGHSZXy28G3skua2SmVi/w4yCE6gbODqnTWlg7+wC604ydGXA8VJiS5ap43JXiUFFAaQ==" >> ~/.ssh/known_hosts
+
+git clone https://github.com/citusdata/release-test-results.git
+
+git config --global user.email "citus-bot@microsoft.com" 
+git config --global user.name "citus bot" 
+
+now=$(date +"%m_%d_%Y_%s")
+
+mv ${hammerdb_dir}/results ${HOME}/release-test-results/hammerdb/${now}
+
+cd ${HOME}/release-test-results
+
+git checkout -b ${rg_name}/${now}
+git add -A 
+git commit -m "add test results for hammerdb tests ${rg_name}"
+git push origin ${rg_name}/${now}

602e3151b1ffab52892080166cf8bc53791c2b74
SaitTalhaNisanci <s.talhanisanci@gmail.com>
2020-02-25 16:54:10 +0300

    add scripts to run tpcc in an automated way
    In order to be able to run the test in an automated way, screen is used
    to create a detached session, github token and username are cached in
    the driver node so that when it needs to access a private repository, it
    can use the tokens.
    
    The create-and-run.sh script will create the cluster and the driver node
    in the same subnet, so that the network latency will be lower.

---
+++ hammerdb/send_pubkey.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# fail if trying to reference a variable that is not set.
+set -u
+# exit immediately if a command fails
+set -e
+# echo commands
+set -x
+
+coordinator_private_ip=$1
+export pub_key=$(cat ~/.ssh/id_rsa.pub)
+ssh -o "StrictHostKeyChecking no" -A ${coordinator_private_ip} "echo ${pub_key} >> ~/.ssh/authorized_keys"

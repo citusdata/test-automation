857e868e2703a2481519b4f12fbd53479eb92daf
SaitTalhaNisanci <s.talhanisanci@gmail.com>
2020-04-16 10:15:47 +0300

    fix directory not exist error

--- hammerdb/setup.sh
+++ hammerdb/setup.sh
@@ -11,21 +11,9 @@ coordinator_ip_address=$1
 username=$2
 
 driverdir="${0%/*}"
-cd "${driverdir}"
 
 hammerdb_version=$(cat ~/HAMMERDB_VERSION)
-
 hammerdb_dir="${HOME}"/HammerDB-"${hammerdb_version}"
-
-sed -i "s/replace_with_ip_address/${coordinator_ip_address}/g" build.tcl
-sed -i "s/replace_with_ip_address/${coordinator_ip_address}/g" run.tcl
-sed -i "s/replace_with_username/${username}/g" sql/ch-benchmark-tables.sql
-
-cp build.tcl "${hammerdb_dir}"/
-cp run.tcl "${hammerdb_dir}"/
-cp ch_benchmark.py "${hammerdb_dir}"/
-
-cp -v ./sql/* "$hammerdb_dir"/
 
 cd "${HOME}"
 
@@ -45,6 +33,18 @@ mv HammerDB/src/postgresql/pgoltp.tcl "${hammerdb_dir}"/src/postgresql/pgoltp.tc
 sudo yum -y install https://download.postgresql.org/pub/repos/yum/reporpms/EL-7-x86_64/pgdg-redhat-repo-latest.noarch.rpm
 sudo yum-config-manager --disable pgdg95
 sudo yum -y install postgresql12-server postgresql12
+
+cd "${driverdir}"
+
+sed -i "s/replace_with_ip_address/${coordinator_ip_address}/g" build.tcl
+sed -i "s/replace_with_ip_address/${coordinator_ip_address}/g" run.tcl
+sed -i "s/replace_with_username/${username}/g" sql/ch-benchmark-tables.sql
+
+cp build.tcl "${hammerdb_dir}"/
+cp run.tcl "${hammerdb_dir}"/
+cp ch_benchmark.py "${hammerdb_dir}"/
+
+cp -v ./sql/* "$hammerdb_dir"/
 
 cd "${hammerdb_dir}"
 

707782eb3373a28e739bd5826a746d4793580d43
Sait Talha Nisanci <s.talhanisanci@gmail.com>
2020-05-04 15:49:59 +0300

    Add a job to delete resource group for hammerdb automatically
    A job to delete a resource group added. If a branch starts with
    delete_resource_group/ it will delete the resource group by extracting
    it from the branch name.
    
    Hammerdb job will push a branch to test automation to trigger this job
    so that we don't need to manually delete the resource group after
    hammerdb benchmarks are done.
    
    For example, if the resource group name is citusbot_resource_group_name,
    hammerdb will push the following branch to test automation after
    completion: delete_resource_group/<id>_citusbot_resource_group_name and
    the job will extract the resource group name to delete it.

---
+++ azure/delete-resource-group-job.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# fail if trying to reference a variable that is not set.
+set -u
+# exit immediately if a command fails
+set -e
+
+BRANCH=${CIRCLE_BRANCH}
+
+# the resource group name will be after the prefix "delete_resource_group/id_"
+# example branch: delete_resource_group/1231123_ch_benchmark_resource_group
+resource_group_name=$(echo "$BRANCH" | sed -e "s@delete_resource_group/[0-9]\+_@@g")
+
+export RESOURCE_GROUP_NAME="$resource_group_name"
+./delete-resource-group.sh
+
---
+++ hammerdb/delete-resource-group.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# fail if trying to reference a variable that is not set.
+set -u
+# exit immediately if a command fails
+set -e
+# echo commands
+set -x
+
+cluster_rg=$1
+
+cd /tmp
+
+# add github to known hosts
+echo "github.com ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAQEAq2A7hRGmdnm9tUDbO9IDSwBK6TbQa+PXYPCPy6rbTrTtw7PHkccKrpp0yVhp5HdEIcKr6pLlVDBfOLX9QUsyCOV0wzfjIJNlGEYsdlLJizHhbn2mUjvSAHQqZETYP81eFzLQNnPHt4EVVUh7VfDESU84KezmD5QlWpXLmvU31/yMf+Se8xhHTvKSCZIFImWwoG6mbUoWf9nzpIoaSjB+weqqUUmpaaasXVal72J+UX2B+2RPW3RcT0eOzQgqlJL3RKrTJvdsjE3JEAvGq3lGHSZXy28G3skua2SmVi/w4yCE6gbODqnTWlg7+wC604ydGXA8VJiS5ap43JXiUFFAaQ==" >> ~/.ssh/known_hosts
+
+git clone https://github.com/citusdata/test-automation.git
+
+git config --global user.email "citus-bot@microsoft.com" 
+git config --global user.name "citus bot"
+
+# use the current second as id
+id=$(date +"%s")
+
+cd test-automation
+git checkout -b delete_resource_group/"${id}"_"${cluster_rg}"
+git push origin delete_resource_group/"${id}"_"${cluster_rg}"
+

e21e312661480285461ed55c54d6b9698a3b1a5f
Sait Talha Nisanci <s.talhanisanci@gmail.com>
2020-05-07 19:05:45 +0300

    test

--- hammerdb/create-run.sh
+++ hammerdb/create-run.sh
@@ -8,7 +8,7 @@ set -e
 set -x
 
 is_tpcc=${IS_TPCC:=true} # set to true if you want tpcc to be run, otherwise set to false
-is_ch=${IS_CH:=true} # set to true if you want ch benchmark to be run, otherwise set to false
+is_ch=${IS_CH:=false} # set to true if you want ch benchmark to be run, otherwise set to false
 username=pguser # username of the database
 hammerdb_version=3.3
 

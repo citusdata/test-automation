44c5f23983490bcbf7a0c318e87ac0097152ea45
Sait Talha Nisanci <s.talhanisanci@gmail.com>
2021-02-03 23:24:12 +0300

    test old versions

--- hammerdb/run.tcl
+++ hammerdb/run.tcl
@@ -19,13 +19,13 @@ diset tpcc pg_defaultdbase pguser
 diset tpcc pg_storedprocs true
 diset tpcc pg_driver timed
 diset tpcc pg_rampup 3
-diset tpcc pg_duration 60
+diset tpcc pg_duration 20
 diset tpcc pg_timeprofile false
 diset tpcc pg_allwarehouse true
 diset tpcc pg_keyandthink false
 loadscript
 print dict
-vuset vu 250
+vuset vu 200
 vuset timestamps 1
 vuset logtotemp 1
 vuset showoutput 0

8749945e66fa81915bb5716f358b0bd2315a88b6
naisila <nicypp@gmail.com>
2021-02-09 23:34:09 +0300

    Issue2pc pgbench

--- release/update_files.sh
+++ release/update_files.sh
@@ -8,9 +8,10 @@ set -e
 set -x
 
 ## VARIABLES ##
-new_branch1=release-9.3
-new_branch2=release-9.2
-new_pg_version=12.2
+new_branch1=master
+new_branch2=issue2pc
+new_pg_version=13.1
+new_use_enterprise=off
 ## VARIABLES ##
 
 releasedir="${0%/*}"
@@ -25,3 +26,8 @@ find . -type f -name "*.ini" |
 # postgres_citus_versions: [('<new_pg_version>', '<new_branch1>')]
 find . -type f -name "*.ini" | 
     xargs sed -i "s@postgres_citus_versions: \[('[^,;]\+', '[^,;]\+')\]@postgres_citus_versions: \[('${new_pg_version}', '${new_branch1}')\]@g"
+# replace all .ini config files (except master.ini which requires use_enterprise to be off):
+# use_enterprise: <use_enterprise>
+# use_enterprise: <new_use_enterprise>
+find . -type f -name "*.ini" ! -name "master.ini" | 
+    xargs sed -i "s@use_enterprise: [^,;]\+@use_enterprise: ${new_use_enterprise}@g"

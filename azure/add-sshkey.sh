8eae6514de2e2d5d59b63b42bc60fa339d899c5e
Nils Dijk <nils@citusdata.com>
2022-01-31 14:30:42 +0100

    use installed key

--- azure/add-sshkey.sh
+++ azure/add-sshkey.sh
@@ -6,6 +6,6 @@ set -u
 set -e
 
 eval `ssh-agent -s`
-ssh-add
+ssh-add ~/.ssh/id_rsa_06b97ddb5954db6d767bf52820fed27d
 
-ssh-keygen -y -f ~/.ssh/id_rsa > ~/.ssh/id_rsa.pub
+ssh-keygen -y -f ~/.ssh/id_rsa_06b97ddb5954db6d767bf52820fed27d > ~/.ssh/id_rsa_06b97ddb5954db6d767bf52820fed27d.pub

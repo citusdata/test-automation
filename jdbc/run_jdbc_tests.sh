89976d50a61c2ca6c2723f546b43d8a580815ef6
gledis69 <zeneliglediss@gmail.com>
2022-06-06 19:20:32 +0300

    Patch for local testing

--- jdbc/run_jdbc_tests.sh
+++ jdbc/run_jdbc_tests.sh
@@ -10,7 +10,7 @@ script_directory="$(dirname "${BASH_SOURCE[0]}")"
 script_directory="$(realpath "${script_directory}")"
 
 # used to parse the json config
-sudo apt-get install -y jq
+apt-get install -y jq
 
 # download the jdbc driver
 jdbc_version=$(cat ./jdbc_config.json | jq '.jdbc_version' | remove_string_quotations)
@@ -18,17 +18,17 @@ jdbc_jar_name="postgresql-${jdbc_version}.jar"
 wget -O $jdbc_jar_name --no-verbose "https://jdbc.postgresql.org/download/${jdbc_jar_name}"
 
 # download java sdk
-sudo apt-get install -y default-jdk
+apt-get install -y default-jdk
 
 cd $HOME
 
 # install pg
-pg_version=$(cat ./jdbc_config.json | jq '.pg_version' | remove_string_quotations)
+pg_version=$(cat $script_directory/jdbc_config.json | jq '.pg_version' | remove_string_quotations)
 install_pg_version $pg_version "--with-openssl"
 
 # get the citus repo
-citus_branch = $(cat ./jdbc_config.json | jq '.citus_branch' | remove_string_quotations)
-use_enterprise = $(cat ./jdbc_config.json | jq '.use_enterprise')
+citus_branch = $(cat $script_directory/jdbc_config.json | jq '.citus_branch' | remove_string_quotations)
+use_enterprise = $(cat $script_directory/jdbc_config.json | jq '.use_enterprise')
 
 if[ "$use_enterprise" == "true" ]; then
   git clone --branch $citus_branch git@github.com:citusdata/citus-enterprise.git citus
@@ -41,53 +41,53 @@ cd citus
 make -sj $(nproc) install
 
 # install citus_dev to setup the cluster
-cd ..
+# cd ..
 
-jdbc_driver_path="./$jdbc_jar_name"
-coordinator_port=9700
+# jdbc_driver_path="./$jdbc_jar_name"
+# coordinator_port=9700
 
 
-# create & run tmp citus cluster for jdbc test 
-citus_dev stop /tmp/jdbc_test_db --port $coordinator_port
-rm -rf /tmp/jdbc_test_db
-mkdir -p /tmp/jdbc_test_db
-citus_dev make /tmp/jdbc_test_db --port $coordinator_port
+# # create & run tmp citus cluster for jdbc test 
+# citus_dev stop /tmp/jdbc_test_db --port $coordinator_port
+# rm -rf /tmp/jdbc_test_db
+# mkdir -p /tmp/jdbc_test_db
+# citus_dev make /tmp/jdbc_test_db --port $coordinator_port
 
-# print installed citus version first
-psql -p $coordinator_port -d $USER -c "select citus_version();"
+# # print installed citus version first
+# psql -p $coordinator_port -d $USER -c "select citus_version();"
 
-# compile java class
-javac JDBCReleaseTest.java
+# # compile java class
+# javac JDBCReleaseTest.java
 
-cd ../tpch_2_13_0
+# cd ../tpch_2_13_0
 
-# clean & build dbgen to generate test data
-make clean
-make
+# # clean & build dbgen to generate test data
+# make clean
+# make
 
-# generate test data
-SCALE_FACTOR=1 CHUNKS="o 2 c 2 P 1 S 2 s 1" sh generate2.sh
+# # generate test data
+# SCALE_FACTOR=1 CHUNKS="o 2 c 2 P 1 S 2 s 1" sh generate2.sh
 
-cd ..
+# cd ..
 
-# perform jdbc tests for combinations of different citus executors & partitioned tables
-for type in hash append
-do
-  for executor in real-time task-tracker adaptive
-  do
-    cd tpch_2_13_0
+# # perform jdbc tests for combinations of different citus executors & partitioned tables
+# for type in hash append
+# do
+#   for executor in real-time task-tracker adaptive
+#   do
+#     cd tpch_2_13_0
 
-    # drop existing tables & create new ones
-    psql -p $coordinator_port -d $USER -f drop_tables.sql -f tpch_create_${type}_partitioned_tables.ddl
+#     # drop existing tables & create new ones
+#     psql -p $coordinator_port -d $USER -f drop_tables.sql -f tpch_create_${type}_partitioned_tables.ddl
 
-    # ingest data
-    for tbl in *.tbl.*
-      do psql -p $coordinator_port -d $USER -c "\COPY ${tbl%.tbl.*} FROM '$tbl' WITH DELIMITER '|'"
-    done
+#     # ingest data
+#     for tbl in *.tbl.*
+#       do psql -p $coordinator_port -d $USER -c "\COPY ${tbl%.tbl.*} FROM '$tbl' WITH DELIMITER '|'"
+#     done
 
-    cd ..
+#     cd ..
 
-    # finally, run jdbc tests for (type x executor) combination
-    CLASSPATH=$jdbc_driver_path:jdbc java JDBCReleaseTest $executor $USER $coordinator_port > ${type}_${executor}
-  done
-done
+#     # finally, run jdbc tests for (type x executor) combination
+#     CLASSPATH=$jdbc_driver_path:jdbc java JDBCReleaseTest $executor $USER $coordinator_port > ${type}_${executor}
+#   done
+# done
--- jdbc/utils.sh
+++ jdbc/utils.sh
@@ -1,10 +1,5 @@
 function remove_string_quotations()
 {
-    if [ "$#" != 1 ]; then
-        echo "Expected one quoted string argument"
-        exit 1
-    fi
-
     # https://tecadmin.net/bash-remove-double-quote-string/#:~:text=A%20single%20line%20sed%20command%20can%20remove%20quotes,will%20remove%20the%20ending%20quote%20from%20the%20string.
     # https://stackoverflow.com/a/35512655
     sed -e 's/^"//' -e 's/"$//' <<<"${1:-$(</dev/stdin)}"
@@ -12,11 +7,6 @@ function remove_string_quotations()
 
 function install_pg_version()
 {
-    if [ "$#" < 1 ]; then
-        echo "Expected at least one version argument"
-        exit 1
-    fi
-
     pg_version=$1
     pg_name=""postgresql-$pg_version""
     install_flags=${2:-""}

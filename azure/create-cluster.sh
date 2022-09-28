5057ddb40d25d263a8c0739eeb1335dfe9154a02
aykutbozkurt <aykut.bozkurt1995@gmail.com>
2022-09-28 16:42:42 +0300

    use make installcheck

--- azure/create-cluster.sh
+++ azure/create-cluster.sh
@@ -49,6 +49,12 @@ local_public_ip=$(curl https://ipinfo.io/ip)
 CREATE_CLUSTER_COMMAND=(az deployment group create -g ${rg} --template-file azuredeploy.json --parameters @azuredeploy.parameters.json
  --parameters sshPublicKey="${public_key}" branchName="$BRANCH" localPublicIp="$local_public_ip")
 
+# override numberOfWorkers param if it is extension testing
+if [ "$rg" == "citusbot_extension_test_resource_group" ]; then
+    CREATE_CLUSTER_COMMAND+=(--parameters)
+    CREATE_CLUSTER_COMMAND+=(numberOfWorkers=0)
+fi
+
 # if VALGRIND_TEST variable is not exported, set it to 0
 is_valgrind_test=${VALGRIND_TEST:=0}
 

724ce5e262c7798bf46888cd92a1d0d3bc6bd057
Gokhan Gulbiz <ggulbiz@gmail.com>
2023-05-11 13:04:00 +0300

    Add new pgbench configs

---
+++ gokhan-run.sh
@@ -0,0 +1,3 @@
+fab run.pgbench-tests --config-file=pgbench_main_stats_off.ini
+fab run.pgbench-tests --config-file=pgbench_main_stats_on.ini
+fab run.pgbench-tests --config-file=pgbench_perf_improvements_stats_on.ini

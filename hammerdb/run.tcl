2f2051a5bdd3daaa2e2bc5a212d7aabd60a6306b
naisila <nicypp@gmail.com>
2025-01-19 14:38:58 +0300

    Hammerdb benchmark 13.0

--- hammerdb/run.tcl
+++ hammerdb/run.tcl
@@ -19,7 +19,7 @@ diset tpcc pg_defaultdbase pguser
 diset tpcc pg_storedprocs true
 diset tpcc pg_driver timed
 diset tpcc pg_rampup 3
-diset tpcc pg_duration 60
+diset tpcc pg_duration 30
 diset tpcc pg_timeprofile false
 diset tpcc pg_allwarehouse true
 diset tpcc pg_keyandthink false

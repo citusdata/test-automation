3e9dfabd55ffb3ac9853894fc78ca29c7e9feba9
Alper Kocatas <alperkocatas@gmail.com>
2025-12-16 18:36:06 +0300

    Fix init.sh to retry connecting to redhat repositories (#279)
    This PR fixes the problem about `create-cluser.sh` failing to
    automatically provision clusters for testing.
    
    Because azure is deploying resources in parallel, and at the time
    custom-script extension runs init.sh, redhat repositories could not be
    resolved. This is most probably because network resources are not fully
    deployed during that time.
     
    To test the theory, I placed a while loop in the init.sh which just
    tries to install hostname package until it succeeds, or sleeps 10
    seconds if it fails, and retries that some 50 times.
    
    With this patch, `create-cluster.sh` can now succeed.
    
    ---------
    
    Co-authored-by: Alper Kocatas <alperkocatas@microsoft.com>

--- azure/init.sh
+++ azure/init.sh
@@ -13,9 +13,32 @@ set -x
 firewall-cmd --add-port=5432/tcp || true
 firewall-cmd --add-port=3456/tcp || true
 
-
 # fail in a pipeline if any of the commands fails
 set -o pipefail
+
+# retry until yum repos become available, somehow redhat repositories cannot 
+# be resolved until some time since vm provisioning
+max_retries=50
+count=1
+
+while (( count <= max_retries )); do
+    echo "Attempt $count of $max_retries: Installing hostname..."
+    
+    # Try installing the package
+    if yum install -y hostname; then
+        echo "Successfully installed hostname."
+        break
+    else
+        echo "Install failed (exit code $?). Retrying in 10 seconds..."
+        sleep 10
+    fi
+    ((count++))
+done
+
+if (( count > max_retries )); then
+    echo "Failed to install hostname after $max_retries attempts." 
+    exit 1
+fi
 
 # install epel repo
 yum -y install https://dl.fedoraproject.org/pub/epel/epel-release-latest-9.noarch.rpm
